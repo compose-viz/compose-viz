01aeb56189e1bf3c53889cacf4c7ef7588792702
Xyphuz <hansen0715.2@gmail.com>
2022-05-18 15:26:03 +0800

    chore: implement TypeError in init of Extends

--- compose_viz/extends.py
+++ compose_viz/extends.py
@@ -1,12 +1,12 @@
 class Extends:
-    def __init__(self, from_file: str, service_name: str):
-        self._from_file = from_file
+    def __init__(self, service_name: str, from_file: str = None):
         self._service_name = service_name
-
-    @property
-    def from_file(self):
-        return self._from_file
+        self._from_file = from_file
 
     @property
     def service_name(self):
         return self._service_name
+
+    @property
+    def from_file(self):
+        return self._from_file
