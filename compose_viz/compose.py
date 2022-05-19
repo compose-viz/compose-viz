4ca486bc2568ad04dd747eb3310cff64e624be63
Xyphuz <hansen0715.2@gmail.com>
2022-05-19 16:03:05 +0800

    chore: remove unused method

--- compose_viz/compose.py
+++ compose_viz/compose.py
@@ -7,9 +7,6 @@ class Compose:
     def __init__(self, services: List[Service]) -> None:
         self._services = services
 
-    def extract_networks(self) -> List[str]:
-        raise NotImplementedError
-
     @property
     def services(self):
         return self._services
