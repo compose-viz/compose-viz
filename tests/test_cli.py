a71f82572c6bceb0e425567b68967e6c24279dd5
Xyphuz <hansen0715.2@gmail.com>
2022-05-25 17:16:38 +0800

    chore: add examples

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,17 +11,19 @@ runner = CliRunner()
 @pytest.mark.parametrize(
     "test_file_path",
     [
-        "builds/docker-compose",
-        "depends_on/docker-compose",
-        "extends/docker-compose",
-        "links/docker-compose",
-        "networks/docker-compose",
-        "ports/docker-compose",
-        "volumes/docker-compose",
+        "tests/ymls/builds/docker-compose.yml",
+        "tests/ymls/depends_on/docker-compose.yml",
+        "tests/ymls/extends/docker-compose.yml",
+        "tests/ymls/links/docker-compose.yml",
+        "tests/ymls/networks/docker-compose.yml",
+        "tests/ymls/others/docker-compose.yml",
+        "tests/ymls/ports/docker-compose.yml",
+        "tests/ymls/volumes/docker-compose.yml",
+        "examples/full-stack-node-app/docker-compose.yml",
     ],
 )
 def test_cli(test_file_path: str) -> None:
-    input_path = f"tests/ymls/{test_file_path}.yml"
+    input_path = f"{test_file_path}"
     output_path = "compose-viz-test.png"
     result = runner.invoke(cli.app, ["-o", output_path, input_path])
 
