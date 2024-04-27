36d18a630047408fde4a3875fab4ee97f3748768
Cheng Chih Yuan <64208218+kevinjone25@users.noreply.github.com>
2024-04-28 01:34:03 +0800

    Fix #51
    * fix: correcting regex for formatting string #51 
    * ci: add cron.yml
    
    ---------
    
    Co-authored-by: Xyphuz <38447974+wst24365888@users.noreply.github.com>

---
+++ naming.py
@@ -0,0 +1,20 @@
+def revise_naming_convention():
+    name_mapping = {
+        "EnvFile1": "EnvFilePath",
+        "Volume1": "AdditionalVolumeOption",
+        "External": "ExternalVolumeNetwork",
+        "External1": "ExternalConfig",
+    }
+
+    with open("./compose_viz/spec/compose_spec.py", "r") as spec_file:
+        spec_content = spec_file.read()
+
+    for origin_name, new_name in name_mapping.items():
+        spec_content.replace(origin_name, new_name)
+
+    return
+
+
+if __name__ == "__main__":
+    revise_naming_convention()
+    print("Revised naming convention successfully!")
