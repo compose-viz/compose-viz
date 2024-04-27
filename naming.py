3ff77e4fbea8fae9b104a6db18c86acf973454da
Zheng Zhi Yuan <kevinjone25@g.ncu.edu.tw>
2024-04-27 19:01:09 +0800

    fix:

--- naming.py
+++ naming.py
@@ -1,14 +1,14 @@
 def revise_naming_convention():
-    name_mapping ={
-        "EnvFile1" : "EnvFilePath",
-        "Volume1" : "AdditionalVolumeOption",
-        "External" : "ExternalVolumeNetwork",
-        "External1" : "ExternalConfig",
+    name_mapping = {
+        "EnvFile1": "EnvFilePath",
+        "Volume1": "AdditionalVolumeOption",
+        "External": "ExternalVolumeNetwork",
+        "External1": "ExternalConfig",
     }
 
     with open("./compose_viz/spec/compose_spec.py", "r") as spec_file:
         spec_content = spec_file.read()
-    
+
     for origin_name, new_name in name_mapping.items():
         spec_content.replace(origin_name, new_name)
 
@@ -18,10 +18,3 @@ def revise_naming_convention():
 if __name__ == "__main__":
     revise_naming_convention()
     print("Revised naming convention successfully!")
-    
-    
-    
-    
-    
-    
-
